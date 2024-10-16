0ce5e29fedda1a20cb69643dedf81c05bad74dbc
GUIFRE88 <guilherme.fre073@gmail.com>
2024-10-16 12:02:23 -0300

    Adicionado tratativa para buscar valores do webmotors

--- scraping_service/app/services/scrap_service.rb
+++ scraping_service/app/services/scrap_service.rb
@@ -1,29 +1,33 @@
 # frozen_string_literal: true
 
 require 'nokogiri'
-require 'httparty'
+require 'puppeteer-ruby'
 
 class ScrapService
-
-
   def scrape(url)
-    headers = {
-      "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-  
-    response = HTTParty.get(url, headers: headers)
-  
-    if response.code == 200
-      sleep(5)  # Atraso para permitir que o JavaScript carregue (ajuste o tempo conforme necessário)
-  
-      page = Nokogiri::HTML(response)
-      brand = page.at_css('.sc-csjSMh.brVEZY .sc-bwCtUz.gqfNBP')&.text&.strip || "Marca não encontrada"
-      model = page.at_css('.sc-fFeiMQ.gqRUjg')&.text&.strip || "Modelo não encontrado"
-      price = page.at_css('#vehicleSendProposalPrice')&.text&.strip || "Preço não encontrado"
-  
+    Puppeteer.launch(headless: true, args: ['--no-sandbox']) do |browser|
+      page = browser.new_page
+
+      user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0.2 Safari/605.1.15'
+      ]
+      page.set_user_agent(user_agents.sample)
+
+      page.goto(url, wait_until: 'networkidle2')
+
+      if page.title.include?("Error")
+        raise "Failed to retrieve the webpage. Status code: #{page.status}"
+      end
+
+      html = page.content
+      parsed_page = Nokogiri::HTML(html)
+
+      brand = parsed_page.at_css('#VehicleBasicInformationTitle')&.text&.strip || "Marca não encontrada"
+      model = parsed_page.at_css('#VehicleBasicInformationDescription')&.text&.strip || "Modelo não encontrado"
+      price = parsed_page.at_css('#vehicleSendProposalPrice')&.text&.strip || "Preço não encontrado"
+
       { brand: brand, model: model, price: price }
-    else
-      raise "Failed to retrieve the webpage. Status code: #{response.code}"
     end
   end
 end
